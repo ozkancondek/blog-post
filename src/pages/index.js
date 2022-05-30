237fec1f7a29b2408b985b389e94c9fd21597179
ozkancondek <ozkncndek@gmail.com>
2022-05-31 00:45:31 +0200

    BP: Search field component created

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,20 @@
 import Layout from "../components/Layout/Layout";
 import styles from "../styles/Home.module.css";
+import SearchInput from "../components/SearchInput/SearchInput";
 
 export default function Home() {
   return (
     <Layout>
-      <div> ozkan</div>
+      <div className={styles.inputContainer}>
+        <div className={styles.counts}>Found 25 posts</div>
+
+        <div className={styles.input}>
+          <SearchInput
+            placeholder="Filter by word"
+            /* onChange={onInputChange} */
+          />
+        </div>
+      </div>
     </Layout>
   );
 }
